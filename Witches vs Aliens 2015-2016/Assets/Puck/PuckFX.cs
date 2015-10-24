ed5fa5c58eda01ebb5b138b6e1ed3e758cd6e060
Reddeyfish <edrichd@uci.edu>
2015-10-23 22:28:52 -0700

    Added VFX to puck impacts

--- Witches vs Aliens 2015-2016/Assets/Puck/PuckFX.cs
+++ Witches vs Aliens 2015-2016/Assets/Puck/PuckFX.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class PuckFX : MonoBehaviour {
+    public GameObject impactVFXPrefab;
+
     const float fxTime = 1f;
     const float ssfxTime = 0.05f;
     const float ssfxIntensityMultiplier = 0.000075f;
@@ -22,7 +24,7 @@ public class PuckFX : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Bing!");
         ScreenShake.RandomShake(this, ssfxTime, other.relativeVelocity.sqrMagnitude * ssfxIntensityMultiplier);
+        SimplePool.Spawn(impactVFXPrefab, (Vector3)(other.contacts[0].point) + Vector3.back);
     }
 }
