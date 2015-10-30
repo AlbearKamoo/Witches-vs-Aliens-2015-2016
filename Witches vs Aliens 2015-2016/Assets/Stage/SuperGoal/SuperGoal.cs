8109d980b33331b132fb73c386116d3051fd41dc
Reddeyfish <edrichd@uci.edu>
2015-10-29 20:46:10 -0700

    supergoal visuals

--- Witches vs Aliens 2015-2016/Assets/Stage/SuperGoal/SuperGoal.cs
+++ Witches vs Aliens 2015-2016/Assets/Stage/SuperGoal/SuperGoal.cs
@@ -5,8 +5,13 @@ using System.Collections;
 public class SuperGoal : MonoBehaviour {
     Collider2D coll;
     SuperGoal _mirror;
+    SpriteRenderer render;
+    ParticleSystem vfx;
     public SuperGoal mirror { set { _mirror = value; } }
     bool _active = false;
+
+    [SerializeField]
+    protected float lerpInTime;
     public bool active
     {
         get
@@ -20,11 +25,45 @@ public class SuperGoal : MonoBehaviour {
                 if (!_active)
                 {
                     coll.enabled = true;
+
+                    render.enabled = true;
+                    rotateParticlesToTransform();
+                    vfx.playbackSpeed = 1.5f;
+                    Color mainRenderColor = render.color;
+                    vfx.Play();
+                    Callback.DoLerp((float f) => {
+                        render.transform.localScale = new Vector2(f * f * (3 - 2 * f), 1f);
+                        render.color = Color.Lerp(Color.white, mainRenderColor, f);
+                    }, 1f, this).FollowedBy(() => 
+                        {
+                            vfx.playbackSpeed = 1f;
+                            render.transform.localScale = Vector2.one;
+                        }, this);
                 }
             }
             else if (_active)
             {
                 coll.enabled = false;
+                Color mainRenderColor = render.color;
+                vfx.Stop();
+                vfx.playbackSpeed = 3;
+                Vector2 originalLocalPosition = render.transform.localPosition;
+                Callback.DoLerp((float f) =>
+                {
+                    float tweenedF = 6 * (Mathf.Pow(f, 2) - Mathf.Pow(f, 3)) + 1 - f;
+                    render.transform.localScale = new Vector2(tweenedF, 1 + 3 * f);
+                    Debug.Log(3 * tweenedF * (originalLocalPosition.y - 1));
+                    render.transform.localPosition = new Vector2(0f, originalLocalPosition.y + 3 * f * (originalLocalPosition.y - 1));
+                    render.color = Color.Lerp(mainRenderColor, Color.white, f);
+                }, 1f, this).FollowedBy(() => 
+                { 
+                    render.transform.localScale = Vector2.one;
+                    render.transform.localPosition = originalLocalPosition;
+                    render.color = mainRenderColor;
+                    render.enabled = false;
+                    vfx.playbackSpeed = 1;
+
+                }, this); 
             }
             _active = value;
         }
@@ -33,6 +72,8 @@ public class SuperGoal : MonoBehaviour {
     void Awake()
     {
         coll = GetComponent<Collider2D>();
+        render = GetComponentInChildren<SpriteRenderer>();
+        vfx = GetComponentInChildren<ParticleSystem>();
     }
 
 	// Use this for initialization
@@ -45,5 +86,10 @@ public class SuperGoal : MonoBehaviour {
         other.transform.position = _mirror.transform.TransformPoint((transform.InverseTransformPoint(other.transform.position)));
         active = false;
         _mirror.active = false;
+    }
+
+    void rotateParticlesToTransform()
+    {
+        vfx.startRotation = Mathf.Deg2Rad * this.transform.eulerAngles.z;
     }
 }
