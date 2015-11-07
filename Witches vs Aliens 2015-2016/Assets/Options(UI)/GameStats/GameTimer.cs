9a8842a509f2c570ab490e185a1e3370e0968e09
Reddeyfish <edrichd@uci.edu>
2015-11-06 19:24:14 -0800

    Overwatch coloring/background

--- Witches vs Aliens 2015-2016/Assets/Options(UI)/GameStats/GameTimer.cs
+++ Witches vs Aliens 2015-2016/Assets/Options(UI)/GameStats/GameTimer.cs
@@ -6,8 +6,17 @@ public class GameTimer : MonoBehaviour {
 
     const string overtime = "OVERTIME!";
 
+    [SerializeField]
+    protected Color OvertimeTextColor;
+    Color normalTextColor;
+    [SerializeField]
+    protected Color OvertimeOutlineColor;
+    Color normalOutlineColor;
+
     public float timeRemainingSec;
     Text UITimer;
+    Outline timeOutline;
+    Image background;
     Coroutine timer;
     public bool running
     {
@@ -36,17 +45,15 @@ public class GameTimer : MonoBehaviour {
             timeRemainingSec -= Time.deltaTime;
             if (timeRemainingSec < 0)
             {
+                timeRemainingSec = 0;
+                setTime();
                 //stuffs
                 if (!GetComponentInParent<Score>().GameEnd())
                 {
                     //overtime!
                     UITimer.text = overtime;
+                    UITimer.alignment = TextAnchor.MiddleCenter;
                     //and VFX
-                }
-                else
-                {
-                    timeRemainingSec = 0;
-                    setTime();
                 }
                 timer = null;
                 yield break;
@@ -58,11 +65,33 @@ public class GameTimer : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         UITimer = GetComponent<Text>();
+        normalTextColor = UITimer.color;
+        timeOutline = GetComponent<Outline>();
+        normalOutlineColor = timeOutline.effectColor;
         setTime();
 	}
 
+    void Start()
+    {
+        background = transform.parent.GetComponentInChildren<Image>();
+    }
+
     void setTime()
     {
+        Debug.Log(timeRemainingSec);
+        if (timeRemainingSec == 0f)
+        {
+            UITimer.color = OvertimeTextColor;
+            timeOutline.effectColor = OvertimeOutlineColor;
+            background.color = Color.white;
+        }
+        else if (timeRemainingSec < 10)
+        {
+            float lerpValue = (1 - (timeRemainingSec / 10)) / 2;
+            UITimer.color = Color.Lerp(normalTextColor, OvertimeTextColor, lerpValue);
+            timeOutline.effectColor = Color.Lerp(normalOutlineColor, OvertimeOutlineColor, lerpValue);
+            background.color = Color.Lerp(Color.clear, Color.white, lerpValue);
+        }
         UITimer.text = Format.formatMilliseconds(timeRemainingSec);
     }
 }
---
+++ Witches vs Aliens 2015-2016/Assets/Options(UI)/GameStats/UIImageShader.shader
@@ -0,0 +1,56 @@
+﻿Shader "Unlit/UIImageShader"
+{
+	Properties
+	{
+		_ImageTex("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Transparent" "IgnoreProjector"="True" "Queue"="Transparent+1"  "PreviewType"="Plane"}
+		Blend One One
+		ZWrite Off
+		Lighting Off
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv_screen : TEXCOORD1;
+			};
+			sampler2D _ImageTex;
+			
+			float2 toScreen(float2 notScreen) //converts from[-1,1] to [0,1] for a float2
+			{
+				return (notScreen + 1) / 2;
+			}
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv_screen = toScreen(o.vertex.xy / o.vertex.w);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 img = tex2D(_ImageTex, i.uv_screen);
+					
+				return img;
+			}
+			ENDCG
+		}
+	}
+}
