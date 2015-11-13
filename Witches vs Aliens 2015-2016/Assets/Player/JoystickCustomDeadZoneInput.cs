3bbcf19fb4141952545f32fe2258d95017740f93
Reddeyfish <edrichd@uci.edu>
2015-11-12 20:46:08 -0800

    Programming for radial dead zones

---
+++ Witches vs Aliens 2015-2016/Assets/Player/JoystickCustomDeadZoneInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickCustomDeadZoneInput : JoystickPlayerInput {
+    [SerializeField]
+    protected float deadZone;
+    float deadZoneSqr;
+    protected void Awake()
+    {
+        deadZoneSqr = Mathf.Pow(deadZone, 2);
+    }
+    protected override void updateAim()
+    {
+        Vector2 aimingInput = new Vector2(Input.GetAxis(bindings.horizontalAimingAxisName), Input.GetAxis(bindings.verticalAimingAxisName));
+        if (aimingInput.sqrMagnitude > deadZoneSqr)
+            action.aimingInputDirection = aimingInput;
+        else
+            action.aimingInputDirection = Vector2.zero;
+    }
+
+    protected override void updateMovement()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxis(bindings.horizontalMovementAxisName), Input.GetAxis(bindings.verticalMovementAxisName));
+        if (movementInput.sqrMagnitude > deadZoneSqr)
+            action.normalizedMovementInput = Vector2.ClampMagnitude(movementInput, 1);
+        else
+            action.normalizedMovementInput = Vector2.zero;
+    }
+}
