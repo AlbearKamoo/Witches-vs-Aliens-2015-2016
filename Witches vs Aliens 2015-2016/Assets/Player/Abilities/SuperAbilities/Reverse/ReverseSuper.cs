688676aa9ab86c64b30676e780a98a8fdba54df3
Reddeyfish <edrichd@uci.edu>
2015-11-16 23:16:42 -0800

    Added reverse controls super

---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/SuperAbilities/Reverse/ReverseSuper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReverseSuper : SuperAbility, IOpponentsAbility {
+
+    [SerializeField]
+    protected float duration;
+
+    List<Transform> _opponents;
+    public List<Transform> opponents { set { _opponents = value; } }
+
+    protected override void onFire(Vector2 direction)
+    {
+        ready = false;
+        active = true;
+        Swap();
+
+        Callback.FireAndForget(Swap, duration, this);
+    }
+
+    void Swap()
+    {
+        foreach (Transform opponent in _opponents)
+        {
+            AbstractPlayerInput input = opponent.GetComponent<AbstractPlayerInput>();
+            string temp = input.bindings.horizontalMovementAxisName;
+            input.bindings.horizontalMovementAxisName = input.bindings.verticalMovementAxisName;
+            input.bindings.verticalMovementAxisName = temp;
+        }
+    }
+}
