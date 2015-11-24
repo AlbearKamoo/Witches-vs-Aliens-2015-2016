194f1a5bee183074c6fcdd4a18f3b99de5295e83
Reddeyfish <edrichd@uci.edu>
2015-11-23 23:15:11 -0800

    Merge fix

--- Witches vs Aliens 2015-2016/Assets/Player/Abilities/Push (Circle)/PushAbilityCircle.cs
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/Push (Circle)/PushAbilityCircle.cs
@@ -1,54 +1,53 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
-public class PushAbilityCircle : GenericAbility {
-	
-	ParticleSystem vfx;
-	CircleCollider2D coll;
-	PointEffector2D effector;
-	MeshRenderer render;
-	
-	[SerializeField]
-	protected float maxDuration;
-	
-	protected override void OnActivate()
-	{
-		base.OnActivate();
-		vfx.Play();
-		coll.enabled = true;
-		effector.enabled = true;
-		render.enabled = true;
-	}
-	
-	protected override void OnDeactivate()
-	{
-		base.OnDeactivate();
-		vfx.Stop();
-		vfx.Clear();
-		coll.enabled = false;
-		effector.enabled = false;
-		render.enabled = false;
-	}
-	
-	// Use this for initialization
-	void Awake()
-	{
-		vfx = GetComponent<ParticleSystem>();
-		coll = GetComponent<CircleCollider2D>();
-		effector = GetComponent<PointEffector2D>();
-		render = GetComponent<MeshRenderer>();
-	}
-	
-	protected override void onFire(Vector2 direction)
-	{
-		StartCoroutine(UpdateCharge());
-	}
-	
-	public override void StopFire()
-	{
-		active = false;
-		base.StopFire();
-	}
+public class PushAbilityCircle : GenericAbility
+{
+
+    ParticleSystem vfx;
+    CircleCollider2D coll;
+    PointEffector2D effector;
+
+    [SerializeField]
+    protected float maxDuration;
+
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        vfx.Play();
+        coll.enabled = true;
+        effector.enabled = true;
+
+    }
+
+    protected override void OnDeactivate()
+    {
+        base.OnDeactivate();
+        vfx.Stop();
+        vfx.Clear();
+        coll.enabled = false;
+        effector.enabled = false;
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        vfx = GetComponent<ParticleSystem>();
+        coll = GetComponent<CircleCollider2D>();
+        effector = GetComponent<PointEffector2D>();
+    }
+
+    protected override void onFire(Vector2 direction)
+    {
+        StartCoroutine(UpdateCharge());
+    }
+
+    public override void StopFire()
+    {
+        //active = false;
+        base.StopFire();
+    }
 
     void OnTriggerStay2D(Collider2D other)
     {
@@ -57,20 +56,20 @@ public class PushAbilityCircle : GenericAbility {
             other.GetComponent<LastBumped>().setLastBumped(this.transform.parent);
         }
     }
-	
-	private IEnumerator UpdateCharge()
-	{
-		active = true;
-		float duration = 0;
-		while (active)
-		{
-			yield return new WaitForFixedUpdate();
-			duration += Time.fixedDeltaTime;
-			if (duration > maxDuration)
-			{
-				active = false;
-				yield break;
-			}
-		}
-	}
+
+    private IEnumerator UpdateCharge()
+    {
+        active = true;
+        float duration = 0;
+        while (active)
+        {
+            yield return new WaitForFixedUpdate();
+            duration += Time.fixedDeltaTime;
+            if (duration > maxDuration)
+            {
+                active = false;
+                yield break;
+            }
+        }
+    }
 }
