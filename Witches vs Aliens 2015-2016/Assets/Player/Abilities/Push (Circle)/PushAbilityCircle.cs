8db5f34ebd766789b30bb45bc3ee1419289c2423
AlvieHuang <Alvinh7@uci.edu>
2015-11-23 18:17:59 -0800

    Fixed Circle Prefab
    chocolate

--- Witches vs Aliens 2015-2016/Assets/Player/Abilities/Push (Circle)/PushAbilityCircle.cs
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/Push (Circle)/PushAbilityCircle.cs
@@ -1,68 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PushAbilityCircle : GenericAbility {
-	
-	ParticleSystem vfx;
-	CircleCollider2D coll;
-	PointEffector2D effector;
-	MeshRenderer render;
-	
-	[SerializeField]
-	protected float maxDuration;
-	
-	protected override void OnActivate()
-	{
-		base.OnActivate();
-		vfx.Play();
-		coll.enabled = true;
-		effector.enabled = true;
-		render.enabled = true;
-	}
-	
-	protected override void OnDeactivate()
-	{
-		base.OnDeactivate();
-		vfx.Stop();
-		vfx.Clear();
-		coll.enabled = false;
-		effector.enabled = false;
-		render.enabled = false;
-	}
-	
-	// Use this for initialization
-	void Awake()
-	{
-		vfx = GetComponent<ParticleSystem>();
-		coll = GetComponent<CircleCollider2D>();
-		effector = GetComponent<PointEffector2D>();
-		render = GetComponent<MeshRenderer>();
-	}
-	
-	protected override void onFire(Vector2 direction)
-	{
-		StartCoroutine(UpdateCharge());
-	}
-	
-	public override void StopFire()
-	{
-		active = false;
-		base.StopFire();
-	}
-	
-	private IEnumerator UpdateCharge()
-	{
-		active = true;
-		float duration = 0;
-		while (active)
-		{
-			yield return new WaitForFixedUpdate();
-			duration += Time.fixedDeltaTime;
-			if (duration > maxDuration)
-			{
-				active = false;
-				yield break;
-			}
-		}
-	}
+public class PushAbilityCircle : GenericAbility
+{
+
+    ParticleSystem vfx;
+    CircleCollider2D coll;
+    PointEffector2D effector;
+
+    [SerializeField]
+    protected float maxDuration;
+
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        vfx.Play();
+        coll.enabled = true;
+        effector.enabled = true;
+
+    }
+
+    protected override void OnDeactivate()
+    {
+        base.OnDeactivate();
+        vfx.Stop();
+        vfx.Clear();
+        coll.enabled = false;
+        effector.enabled = false;
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        vfx = GetComponent<ParticleSystem>();
+        coll = GetComponent<CircleCollider2D>();
+        effector = GetComponent<PointEffector2D>();
+    }
+
+    protected override void onFire(Vector2 direction)
+    {
+        StartCoroutine(UpdateCharge());
+    }
+
+    public override void StopFire()
+    {
+        //active = false;
+        base.StopFire();
+    }
+
+    private IEnumerator UpdateCharge()
+    {
+        active = true;
+        float duration = 0;
+        while (active)
+        {
+            yield return new WaitForFixedUpdate();
+            duration += Time.fixedDeltaTime;
+            if (duration > maxDuration)
+            {
+                active = false;
+                yield break;
+            }
+        }
+    }
 }
