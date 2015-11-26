47e8771e5179242cab62c42c37f0860043786460
Reddeyfish <edrichd@uci.edu>
2015-11-25 17:00:02 -0800

    added quit button

---
+++ Witches vs Aliens 2015-2016/Assets/Options(UI)/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//allows us to set up scene changes with button inspectors
+
+public class LoadLevel : MonoBehaviour {
+
+    public void LoadSene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
