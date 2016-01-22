04a0f6d66cadc095b7f7898dfce09aeed8b62cad
Reddeyfish <edrichd@uci.edu>
2016-01-22 14:57:13 -0800

    add forward shield ability

---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/GenericAbilities/ForwardShield/ForwardShieldAbility.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardShieldAbility : TimedGenericAbility
+{
+    [SerializeField]
+    protected GameObject forwardShieldPrefab;
+
+    GameObject forwardShield;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        forwardShield = GameObject.Instantiate(forwardShieldPrefab);
+        forwardShield.SetActive(false);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        forwardShield.transform.SetParent(transform.root.Find("Rotating"), false);
+    }
+
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        forwardShield.SetActive(true);
+    }
+
+    protected override void OnDeactivate()
+    {
+        base.OnDeactivate();
+        forwardShield.SetActive(false);
+    }
+}
