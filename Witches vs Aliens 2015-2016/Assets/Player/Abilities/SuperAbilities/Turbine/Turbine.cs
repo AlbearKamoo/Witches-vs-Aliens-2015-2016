49aab907bf5bdafe7f603a64a1090ae4688531d1
Reddeyfish <edrichd@uci.edu>
2016-02-17 15:31:57 -0800

    turbine super

---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/SuperAbilities/Turbine/Turbine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Turbine : MonoBehaviour {
+
+    [SerializeField]
+    protected float rotationSpeed;
+
+    public bool active
+    {
+        set
+        {
+            this.gameObject.SetActive(value);
+            if (value)
+            {
+                rigid.angularVelocity = rotationSpeed;
+            }
+        }
+    }
+
+    Rigidbody2D rigid;
+    Collider2D[] myColliders;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        myColliders = GetComponentsInChildren<Collider2D>();
+        active = true;
+    }
+
+    public void ignoreCollisions(List<Collider2D> colliders)
+    {
+        for (int i = 0; i < myColliders.Length; i++)
+        {
+            for (int j = 0; j < colliders.Count; j++)
+            {
+                Physics2D.IgnoreCollision(myColliders[i], colliders[j]);
+            }
+        }
+    }
+}
---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/SuperAbilities/Turbine/TurbineSuper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurbineSuper : SuperAbility, IAlliesAbility
+{
+
+    [SerializeField]
+    protected GameObject turbinePrefab;
+
+    [SerializeField]
+    protected float duration;
+
+    List<Collider2D> allyColliders;
+    public List<Transform> allies 
+    { 
+        set
+        {
+            allyColliders = new List<Collider2D>();
+            for (int i = 0; i < value.Count; i++)
+            {
+                allyColliders.AddRange(value[i].GetComponentsInChildren<Collider2D>());
+            }
+        }
+    }
+
+    Turbine turbine;
+
+    protected override void Start()
+    {
+        turbine = Instantiate(turbinePrefab).GetComponent<Turbine>();
+        base.Start();
+        ready = true; //for easy testing
+        turbine.active = false;
+    }
+
+    protected override void onFire(Vector2 direction)
+    {
+        ready = false;
+
+        turbine.active = true;
+
+        turbine.ignoreCollisions(allyColliders);
+
+        Callback.FireAndForget(() => turbine.active = false, duration, this);
+    }
+}
