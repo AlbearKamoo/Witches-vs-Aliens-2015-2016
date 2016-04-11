a71b8bcc136a0df37b1a72efc448cb582438d73a
Reddeyfish <edrichd@uci.edu>
2016-04-10 22:37:57 -0700

    added tooltip for supers

--- Witches vs Aliens 2015-2016/Assets/Player/Abilities/SuperAbility.cs
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/SuperAbility.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SuperAbility : AbstractAbility
 {
+    const float tooltipDelay = 3f;
+
     public override AbilityType type { get { return AbilityType.SUPER; } }
     bool _ready = false;
     public override bool ready {
@@ -10,16 +12,32 @@ public class SuperAbility : AbstractAbility
         set
         {
             visuals.SetActive(value);
+
+            tooltip.SetActive(false);
+            if (value)
+                Callback.FireAndForget(() => { if (ready) tooltip.SetActive(true); }, tooltipDelay, this);
+
             _ready = value;
         }
     }
 
     GameObject visuals;
-
+    GameObject tooltip;
     protected virtual void Start()
     {
-        if(transform.parent != null)
+        if (transform.parent != null)
+        {
             visuals = transform.parent.Find("SuperUI").gameObject;
+            switch (transform.root.GetComponent<AbstractPlayerInput>().bindings.inputMode)
+            {
+                case InputConfiguration.PlayerInputType.MOUSE:
+                    tooltip = transform.parent.Find("SuperUI/Tooltips/Mouse Tooltip").gameObject;
+                    break;
+                case InputConfiguration.PlayerInputType.JOYSTICK:
+                    tooltip = transform.parent.Find("SuperUI/Tooltips/Joystick Tooltip").gameObject;
+                    break;
+            }
+        }
     }
 
     protected override void onFire(Vector2 direction)
