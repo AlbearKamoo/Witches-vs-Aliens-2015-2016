7b431021edf94603505d90e81518b341279ce4cb
Reddeyfish <edrichd@uci.edu>
2016-04-13 15:02:03 -0700

    add mirror11 ability

---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/GenericAbilities/Mirror11/Mirror11.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VisualAnimate))]
+public class Mirror11 : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject physics;
+
+    [SerializeField]
+    protected float inactiveAlpha;
+
+    AbstractPlayerVisuals visuals;
+
+    bool _active;
+    public bool active
+    {
+        get
+        {
+            return _active;
+        }
+        set
+        {
+            physics.SetActive(_active = value);
+            if (_active)
+            {
+                visuals.alpha = 1;
+            }
+            else
+            {
+                visuals.alpha = inactiveAlpha;
+            }
+        }
+    }
+
+    public void Initialize(Transform root, Vector3 localPosition)
+    {
+        visuals = Instantiate(root.GetComponentInChildren<AbstractPlayerVisuals>().gameObject).GetComponent<AbstractPlayerVisuals>();
+        visuals.transform.SetParent(this.transform, false);
+        visuals.transform.localPosition = Vector3.zero;
+        this.transform.SetParent(root, false);
+        this.transform.localPosition = localPosition;
+    }
+}
---
+++ Witches vs Aliens 2015-2016/Assets/Player/Abilities/GenericAbilities/Mirror11/Mirror11Ability.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror11Ability : TimedGenericAbility
+{
+
+    [SerializeField]
+    protected GameObject mirrorPrefab;
+    const float xSeperation = 25;
+
+    Mirror11[] instantiatedMirrors;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        instantiatedMirrors = new Mirror11[2];
+        instantiatedMirrors[0] = Instantiate(mirrorPrefab).GetComponent<Mirror11>();
+        instantiatedMirrors[0].Initialize(transform.root, xSeperation * Vector2.right);
+        instantiatedMirrors[0].active = false;
+
+        instantiatedMirrors[1] = Instantiate(mirrorPrefab).GetComponent<Mirror11>();
+        instantiatedMirrors[1].Initialize(transform.root, xSeperation * Vector2.left);
+        instantiatedMirrors[1].active = false;
+    }
+
+    void setMirrorsActive(bool active)
+    {
+        for (int i = 0; i < instantiatedMirrors.Length; i++)
+            instantiatedMirrors[i].active = active;
+    }
+
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        setMirrorsActive(true);
+    }
+
+    protected override void OnDeactivate()
+    {
+        base.OnDeactivate();
+        setMirrorsActive(false);
+    }
+}
