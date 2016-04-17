77c71063e22f8725e225b887caa4b834e4ed3984
Reddeyfish <edrichd@uci.edu>
2016-04-16 21:56:02 -0700

    Finished 2D noisemaker

--- Witches vs Aliens 2015-2016/Assets/Utility/Non-Runtime Scripts/OneDTo2DNoise.cs
+++ Witches vs Aliens 2015-2016/Assets/Utility/Non-Runtime Scripts/OneDTo2DNoise.cs
@@ -9,12 +9,19 @@ public class OneDTo2DNoise : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        for (int x = 0; x < OneDNoiseTexture.width; x++)
+        {
+            for (int y = 0; y < OneDNoiseTexture.height; y++)
+            {
+                Color originalColor = OneDNoiseTexture.GetPixel(x, y);
+                Color rotatedColor = OneDNoiseTexture.GetPixel(y, 1-x);
+                Color newColor = new Color(originalColor.r, rotatedColor.r, 0);
+                OneDNoiseTexture.SetPixel(x, y, newColor);
+            }
+        }
+        OneDNoiseTexture.Apply();
+
         byte[] bytes = OneDNoiseTexture.EncodeToPNG();
         File.WriteAllBytes(Application.dataPath + "/ScriptingOutput/New 2DNoise.png", bytes);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
