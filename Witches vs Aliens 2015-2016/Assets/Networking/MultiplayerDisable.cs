e04e4dd6cb6daed5f38a3d3872914cca26294458
Reddeyfish <edrichd@uci.edu>
2016-04-20 18:28:56 -0700

    pause menu disabled with multiplayer

---
+++ Witches vs Aliens 2015-2016/Assets/Networking/MultiplayerDisable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerDisable : MonoBehaviour {
+
+    [SerializeField]
+    protected bool disableWithMultiplayer;
+
+	// Use this for initialization
+	void Start () {
+        if (disableWithMultiplayer)
+        {
+            if (NetworkNode.node != null)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (NetworkNode.node == null)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+	}
+}
