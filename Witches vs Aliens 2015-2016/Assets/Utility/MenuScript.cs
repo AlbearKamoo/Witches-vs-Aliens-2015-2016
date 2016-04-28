20a5d19922b8104515e6ebb4193a1426ddbf1a8d
AlbearKamoo <mrautran@gmail.com>
2016-04-27 23:11:29 -0700

    Rudimentary main menu implementation

---
+++ Witches vs Aliens 2015-2016/Assets/Utility/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void PlayLocal()
+    {
+        SceneManager.LoadScene("PlayerRegistration");
+    }
+
+    public void PlayLAN()
+    {
+        SceneManager.LoadScene("GameSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
