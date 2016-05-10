ce4b1eacc1096b27898ab5af6225fd7e2a565e2f
Reddeyfish <edrichd@uci.edu>
2016-05-10 13:37:42 -0700

    refactored portions of cloud generation

--- Witches vs Aliens 2015-2016/Assets/Utility/Non-Runtime Scripts/CloudGenerator.cs
+++ Witches vs Aliens 2015-2016/Assets/Utility/Non-Runtime Scripts/CloudGenerator.cs
@@ -8,8 +8,10 @@ public class CloudGenerator : MonoBehaviour
     protected Vector2 dimensions;
 
     [SerializeField]
-    protected Vector2[] noiseScales; //scale, weight
+    protected OctaveStruct[] octaves;
 
+    [SerializeField]
+    protected string filename = "New Cloud.png";
     // Use this for initialization
     void Start()
     {
@@ -17,15 +19,10 @@ public class CloudGenerator : MonoBehaviour
         int height = Mathf.RoundToInt(dimensions.x);
 
         float max = 0;
-        float[] seeds = new float[8 * noiseScales.Length];
-        for (int i = 0; i < noiseScales.Length; i++)
-        {
-            max += noiseScales[i].y;
-        }
 
-        for (int i = 0; i < seeds.Length; i++)
+        for (int i = 0; i < octaves.Length; i++)
         {
-            seeds[i] = Random.Range(-99999, 99999);
+            max += octaves[i].weight;
         }
 
             if (max == 0)
@@ -35,54 +32,54 @@ public class CloudGenerator : MonoBehaviour
 
         Texture2D result = new Texture2D(width, height);
 
+        float[, ,] resultArray = new float[width, height, 4];
+
+        for(int x = 0; x < width; x++)
+            for(int y = 0; y < width; y++)
+                for(int c = 0; c < 4; c++)
+                    resultArray[x, y, c] = 0;
+
+        for(int i = 0; i < octaves.Length; i++)        
+        {
+            float[, ,] addition = octaves[i].octave.generateOctave(width, height);
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < width; y++)
+                    for (int c = 0; c < 4; c++)
+                        resultArray[x, y, c] += octaves[i].weight * addition[x, y, c];
+
+        }
+
         for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < width; y++)
             {
-                for (int y = 0; y < height; y++)
+                for (int c = 0; c < 4; c++)
                 {
-                    float xLerp = (float)x / width;
-                    float yLerp = (float)y / width;
-
-                    float OneMinXLerp = 1 - xLerp;
-                    float OneMinYLerp = 1 - yLerp;
-
-                    float valueXTilable = 0;
-                    float valueYTilable = 0;
-
-                    float valueXUntilable = 0;
-                    float valueYUntilable = 0;
-
-                    for (int i = 0; i < noiseScales.Length; i++)
-                    {
-                        valueXTilable += noiseScales[i].y * (xLerp + yLerp) * Mathf.PerlinNoise(seeds[8 * i + 0] + noiseScales[i].x * xLerp, seeds[8 * i + 1] + noiseScales[i].x * yLerp);
-                        valueXTilable += noiseScales[i].y * (xLerp + OneMinYLerp) * Mathf.PerlinNoise(seeds[8 * i + 0] + noiseScales[i].x * xLerp, seeds[8 * i + 1] + noiseScales[i].x * OneMinYLerp);
-                        valueXTilable += noiseScales[i].y * (OneMinXLerp + yLerp) * Mathf.PerlinNoise(seeds[8 * i + 0] + noiseScales[i].x * OneMinXLerp, seeds[8 * i + 1] + noiseScales[i].x * yLerp);
-                        valueXTilable += noiseScales[i].y * (OneMinXLerp + OneMinYLerp) * Mathf.PerlinNoise(seeds[8 * i + 0] + noiseScales[i].x * OneMinXLerp, seeds[8 * i + 1] + noiseScales[i].x * OneMinYLerp);
-
-                        valueYTilable += noiseScales[i].y * (xLerp + yLerp) * Mathf.PerlinNoise(seeds[8 * i + 2] + noiseScales[i].x * xLerp, seeds[8 * i + 3] + noiseScales[i].x * yLerp);
-                        valueYTilable += noiseScales[i].y * (xLerp + OneMinYLerp) * Mathf.PerlinNoise(seeds[8 * i + 2] + noiseScales[i].x * xLerp, seeds[8 * i + 3] + noiseScales[i].x * OneMinYLerp);
-                        valueYTilable += noiseScales[i].y * (OneMinXLerp + yLerp) * Mathf.PerlinNoise(seeds[8 * i + 2] + noiseScales[i].x * OneMinXLerp, seeds[8 * i + 3] + noiseScales[i].x * yLerp);
-                        valueYTilable += noiseScales[i].y * (OneMinXLerp + OneMinYLerp) * Mathf.PerlinNoise(seeds[8 * i + 2] + noiseScales[i].x * OneMinXLerp, seeds[8 * i + 3] + noiseScales[i].x * OneMinYLerp);
-
-                        valueXUntilable += noiseScales[i].y * Mathf.PerlinNoise(seeds[8 * i + 4] + noiseScales[i].x * xLerp, seeds[8 * i + 5] + noiseScales[i].x * yLerp);
-                        valueYUntilable += noiseScales[i].y * Mathf.PerlinNoise(seeds[8 * i + 6] + noiseScales[i].x * xLerp, seeds[8 * i + 7] + noiseScales[i].x * yLerp);
-                    }
-
-                    valueXTilable /= (4 * max);
-                    valueYTilable /= (4 * max);
-
-                    valueXUntilable /= max;
-                    valueYUntilable /= max;
-
-                    float valueX = Mathf.Lerp(valueXTilable, valueXUntilable, Mathf.Min(2 * Mathf.Min(xLerp, OneMinXLerp), 2 * Mathf.Min(yLerp, OneMinYLerp)));
-                    float valueY = Mathf.Lerp(valueYTilable, valueYUntilable, Mathf.Min(2 * Mathf.Min(xLerp, OneMinXLerp), 2 * Mathf.Min(yLerp, OneMinYLerp)));
-
-                    Color newColor = new Color(valueX, valueY, 0);
-                    result.SetPixel(x, y, newColor);
+                    Color resultColor = new Color(resultArray[x, y, 0] / max, resultArray[x, y, 1] / max, resultArray[x, y, 2] / max, resultArray[x, y, 3] / max);
+                    result.SetPixel(x, y, resultColor);
                 }
             }
+        }
+
         result.Apply();
 
         byte[] bytes = result.EncodeToPNG();
-        File.WriteAllBytes(Application.dataPath + "/ScriptingOutput/New Cloud.png", bytes);
+        File.WriteAllBytes(Application.dataPath + "/ScriptingOutput/" + filename, bytes);
     }
+}
+
+[System.Serializable]
+public struct OctaveStruct
+{
+    public CloudOctave octave;
+    public float weight;
+}
+
+public abstract class CloudOctave : MonoBehaviour
+{
+    [SerializeField]
+    protected float frequency;
+
+    public abstract float[, ,] generateOctave(int width, int height);
+
 }
---
+++ Witches vs Aliens 2015-2016/Assets/Utility/Non-Runtime Scripts/PerlinCloudOctave.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerlinCloudOctave : CloudOctave {
+
+    public override float[, ,] generateOctave(int width, int height)
+    {
+        float[, ,] result = new float[width, height, 4];
+
+        float[,] seeds = new float[4, 4];
+
+        for (int x = 0; x < 4; x++)
+            for (int y = 0; y < 4; y++)
+                seeds[x, y] = Random.Range(-99999, 99999);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xLerp = (float)x / width;
+                float yLerp = (float)y / width;
+
+                float xLerpShifted = Mathf.Abs(xLerp - 0.5f);
+                float yLerpShifted = Mathf.Abs(yLerp - 0.5f);
+
+                for (int c = 0; c < 4; c++)
+                {
+                    float tilable = 0;
+                    float untilable = 0;
+
+                    tilable += Mathf.PerlinNoise(seeds[c, 0] + frequency * xLerpShifted, seeds[c, 1] + frequency * yLerpShifted);
+
+                    untilable += Mathf.PerlinNoise(seeds[c, 2] + frequency * xLerp, seeds[c, 3] + frequency * yLerp);
+
+                    float value = Mathf.Lerp(tilable, untilable, Mathf.Min(2 * Mathf.Min(xLerp, 1 - xLerp), 2 * Mathf.Min(yLerp, 1 - yLerp)));
+
+                    result[x, y, c] = value;
+
+                }
+            }
+        }
+
+        return result;
+    }
+}
