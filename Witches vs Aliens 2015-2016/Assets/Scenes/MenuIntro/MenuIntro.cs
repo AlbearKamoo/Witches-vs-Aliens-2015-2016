16b40f310e8fed885d865c3a9e156e2d25d9709e
Reddeyfish <edrichd@uci.edu>
2016-05-25 17:15:08 -0700

    add format for intro cinematic

---
+++ Witches vs Aliens 2015-2016/Assets/Scenes/MenuIntro/MenuIntro.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuIntro : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject menuMusic;
+
+    static bool disabled = false;
+
+	// Use this for initialization
+	void Start () {
+        if (disabled)
+        {
+            Instantiate(menuMusic);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            disabled = true;
+            Callback.FireAndForget(() => {
+                Instantiate(menuMusic);
+                Destroy(this.gameObject);
+            }, 5, this);
+        }
+	}
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Instantiate(menuMusic);
+            Destroy(this.gameObject);
+        }
+    }
+}
